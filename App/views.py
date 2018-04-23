8b6bf7c7e669a88633eb63f0c1acd09c79c779d1
jiang <320235356@qqcom>
2018-04-23 06:39:57 -0700

    项目搭建2

--- App/views.py
+++ App/views.py
@@ -3,7 +3,7 @@
 from flask import Blueprint
 
 
-from App.models import Person
+# from App.models import Person
 
 from App.ext import db
 
@@ -18,32 +18,32 @@ def index():
     return 'hello Index'
 
 
-# 创建表
-@blue.route('/createdb/')
-def create_db():
-    db.create_all()
-
-    return 'su'
-
-
-# 删除表
-@blue.route('/dropdb/')
-def drop_db():
-    db.drop_all()
-    return 'su'
-
-
-
-
-# 添加数据
-@blue.route('/addperson/')
-def add_person():
-    p = Person()
-
-    p.p_age=15
-    p.p_name="二狗"
-
-    # 存到数据库
-    db.session.add(p)
-    db.session.commit()
-    return 'su'
+# # 创建表
+# @blue.route('/createdb/')
+# def create_db():
+#     db.create_all()
+#
+#     return 'su'
+#
+#
+# # 删除表
+# @blue.route('/dropdb/')
+# def drop_db():
+#     db.drop_all()
+#     return 'su'
+#
+#
+#
+#
+# # 添加数据
+# @blue.route('/addperson/')
+# def add_person():
+#     p = Person()
+#
+#     p.p_age=15
+#     p.p_name="二狗"
+#
+#     # 存到数据库
+#     db.session.add(p)
+#     db.session.commit()
+#     return 'su'
